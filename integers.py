0fc0915602a5a8b58621ccfd1bfb76af1b4ee056
prowesspeace <peaceibelegbu2@gmail.com>
2022-05-26 15:41:08 -0600

    script git commit

---
+++ integers.py
@@ -0,0 +1,4 @@
+number1 = raw_input('Write one number:  ')
+number2 = raw_input('Write other number: ')
+result =  number1 + number2
+print "The sum of the numbers is: %d" % result # here, display the result of the operatio n.
