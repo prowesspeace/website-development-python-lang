54793eb002d2019e8f735b31fe1c599bffff5d98
prowesspeace <peaceibelegbu2@gmail.com>
2022-05-28 16:23:18 -0600

    see added number

--- integers.py
+++ integers.py
@@ -1,4 +1,4 @@
 number1 = raw_input('Write one number:  ')
 number2 = raw_input('Write other number: ')
-result =  number1 + number2
+result =  number1 + number2 + number6
 print "The sum of the numbers is: %d" % result # here, display the result of the operatio n.
