1b06e4af7777699c2c5bc6ea468d54c000699629
prowesspeace <peaceibelegbu2@gmail.com>
2022-05-28 16:42:25 -0600

    see modification

--- integers.py
+++ integers.py
@@ -3,4 +3,5 @@ number2 = raw_input('Write other number: ')
 result =  number1 + number2 + number6
 print "The sum of the numbers is: %d" % result # here, display the result of the operatio n.
 print "The sum of the numbers is: number6 + number3'
+print "revoke the sum of the numbers"
 
